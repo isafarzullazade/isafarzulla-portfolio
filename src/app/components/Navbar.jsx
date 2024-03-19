0638934da9a7665935163517f46b4aecae9c6b57
Isa Farzullazade <isafarzullazade@Isas-MacBook-Pro.local>
2024-03-18 23:22:48 -0700

    title

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
+import Head from "next/head";
 
 const navLinks = [
   {
@@ -25,6 +26,10 @@ const Navbar = () => {
 
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+     
+     <Head>
+        <title>Isa Farzulla</title>
+      </Head>
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
